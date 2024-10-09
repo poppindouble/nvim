2a2a55db64043a0e1a2eec44a19e715785f31983
poppindouble <poppindouble@gmail.com>
2024-10-09 17:04:18 +0900

    fix conflicting mapping

--- init.lua
+++ init.lua
@@ -364,7 +364,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<C-f>', builtin.find_files, { desc = 'Files' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>f', builtin.live_grep, { desc = 'Grep' })
+      vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = '[G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
